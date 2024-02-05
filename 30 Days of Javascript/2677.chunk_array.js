c6a1d1aa7ad77de52818c9806553a0f952daa643
Tareq <tareqnmd@gmail.com>
2024-02-05 07:21:50 +0600

    chunk updated

--- 30 Days of Javascript/2677.chunk_array.js
+++ 30 Days of Javascript/2677.chunk_array.js
@@ -1,15 +1,33 @@
 const chunk = (arr, size) => {
 	if (arr.length === 0) return [];
+	const arr_length = arr.length;
 	const new_arr = [];
-	for (let index = 0; index < arr.length; index++) {
-		// console.log(index, size, arr.length, +size < +arr.length);
-		console.log(arr, size > arr.length);
-		if (size > arr.length) break;
+	for (let i = 0; i < arr_length; i++) {
+		if (size > arr.length) {
+			if (arr.length > 0) {
+				new_arr.push(arr);
+			}
+			break;
+		}
 		new_arr.push(arr.splice(0, size));
 	}
 	return new_arr;
 };
-console.log(chunk([1, 2, 3, 4, 5], 1));
+
+// let chunk = (arr, size) => {
+// 	const chunkedArray = [];
+// 	let index = 0;
+
+// 	while (index < arr.length) {
+// 		chunkedArray.push(arr.slice(index, index + size));
+// 		index += size;
+// 	}
+
+// 	return chunkedArray;
+// };
+
+
+// console.log(chunk([1, 2, 3, 4, 5], 1));
 // console.log(chunk([1, 9, 6, 3, 2], 3));
 // console.log(chunk([8, 5, 3, 2, 6], 6));
 // console.log(chunk([], 1));
