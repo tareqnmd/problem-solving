d6ea6f63a01df17e9619b893145aa45ca28ab961
Tareq <tareqnmd@gmail.com>
2024-02-06 15:07:05 +0600

    method chain

---
+++ 30 Days of Javascript/2726.calculator_with_method_chaining.js
@@ -0,0 +1,75 @@
+class Calculator {
+	constructor(value) {
+		this.value = value;
+	}
+
+	add(value) {
+		this.value += value;
+		return this;
+	}
+
+	subtract(value) {
+		this.value -= value;
+		return this;
+	}
+
+	multiply(value) {
+		this.value *= value;
+		return this;
+	}
+
+	divide(value) {
+		if (value === 0) {
+			throw Error('Division by zero is not allowed');
+		}
+		this.value /= value;
+		return this;
+	}
+
+	power(value) {
+		this.value **= value;
+		return this;
+	}
+
+	getResult() {
+		return this.value;
+	}
+}
+
+// class Calculator {
+// 	constructor(value) {
+// 		this.value = value;
+// 	}
+
+// 	add(value) {
+// 		this.value = this.value + value;
+// 		return this;
+// 	}
+
+// 	subtract(value) {
+// 		this.value = this.value - value;
+// 		return this;
+// 	}
+
+// 	multiply(value) {
+// 		this.value = this.value * value;
+// 		return this;
+// 	}
+
+// 	divide(value) {
+// 		if (value === 0) {
+// 			throw 'Division by zero is not allowed';
+// 		}
+// 		this.value = this.value / value;
+// 		return this;
+// 	}
+
+// 	power(value) {
+// 		this.value = this.value ** value;
+// 		return this;
+// 	}
+
+// 	getResult() {
+// 		return this.value;
+// 	}
+// }
