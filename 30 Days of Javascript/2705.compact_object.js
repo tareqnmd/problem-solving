fcb726337d66a8b874b0880a99081b2e371c4fa0
Tareq <tareqnmd@gmail.com>
2024-02-11 19:13:14 +0600

    obj com

---
+++ 30 Days of Javascript/2705.compact_object.js
@@ -0,0 +1,17 @@
+var compactObject = function (obj) {
+	for (const property in obj) {
+		if (obj[property]) {
+			if (!['string', 'number', 'boolean'].includes(typeof obj[property])) {
+				compactObject(obj[property]);
+			}
+		} else {
+			delete obj[property];
+		}
+	}
+	return Array.isArray(obj) ? obj.filter((item) => item) : obj;
+};
+
+console.log(compactObject([null, 0, false, 1]));
+// console.log(compactObject({ a: null, b: [false, 1] }));
+// console.log(compactObject([null, 0, 5, [0], [false, 16]]));
+console.log(compactObject([-1, 0, [0, false], true, null]));
