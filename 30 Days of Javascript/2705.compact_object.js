cbd3cf9c59ef19b5c370bb80e2e4b5a447266435
Tareq <tareqnmd@gmail.com>
2024-02-12 12:41:30 +0600

    up

--- 30 Days of Javascript/2705.compact_object.js
+++ 30 Days of Javascript/2705.compact_object.js
@@ -11,6 +11,24 @@ var compactObject = function (obj) {
 	return Array.isArray(obj) ? obj.filter((item) => item) : obj;
 };
 
+var compactObject = function (obj) {
+	if (Array.isArray(obj)) {
+		const falsy_filtered = obj.filter(Boolean);
+		return falsy_filtered.map((item) =>
+			typeof item === 'object' ? compactObject(item) : item
+		);
+	} else {
+		const res = {};
+		for (const key in obj) {
+			if (Boolean(obj[key])) {
+				res[key] =
+					typeof obj[key] === 'object' ? compactObject(obj[key]) : obj[key];
+			}
+		}
+		return res;
+	}
+};
+
 console.log(compactObject([null, 0, false, 1]));
 console.log(compactObject({ a: null, b: [false, 1] }));
 console.log(compactObject([null, 0, 5, [0], [false, 16]]));
