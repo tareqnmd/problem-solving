14aab503b4f6dfb10aee338b98f92f1134ae1e1e
Tareq <tareqnmd@gmail.com>
2024-02-13 17:35:35 +0600

    two-sum

--- Js/1.two-sum.js
+++ Js/1.two-sum.js
@@ -1,8 +1,7 @@
 const twoSum = function (nums, target) {
-	var obj = {};
+	const obj = {};
 	for (let i = 0; i < nums.length; i++) {
-		var n = nums[i];
-
+		const n = nums[i];
 		if (obj[target - n] >= 0) {
 			return [obj[target - n], i];
 		} else {
