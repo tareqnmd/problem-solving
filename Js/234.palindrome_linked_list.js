db16f8d6ecd719dcaba748af6bada600ef346d2a
Tareq <tareqnmd@gmail.com>
2024-02-13 15:22:29 +0600

    palindrome

--- Js/234.palindrome_linked_list.js
+++ Js/234.palindrome_linked_list.js
@@ -1,6 +1,15 @@
-var isPalindrome = function (head) {
-	for (let i = 0; i < parseInt(head.length / 2); i++) {
-		if (head[i] !== head[head.length - 1 - i]) return false;
+var linkedPalindrome = function (head) {
+	const array = [];
+	while (head !== null) {
+		array.push(head.val);
+		head = head.next;
+	}
+	for (let i = 0; i < parseInt(array.length / 2); i++) {
+		if (String(array[i]) !== String(array[array.length - 1 - i])) {
+			return false;
+		}
+		continue;
 	}
 	return true;
+	// return array.join('') === array.reverse().join('');
 };
