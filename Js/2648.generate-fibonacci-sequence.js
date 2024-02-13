89b194e9e0f2cea6efc88c2e0e93a38b06df4714
Tareq <tareqnmd@gmail.com>
2024-02-13 15:59:33 +0600

    grn fibo

---
+++ Js/2648.generate-fibonacci-sequence.js
@@ -0,0 +1,15 @@
+var fibGenerator = function* () {
+	let a = 0;
+	let b = 1;
+	while (true) {
+		yield a;
+		let tmp = a;
+		a = b;
+		b += tmp;
+	}
+};
+
+const gen = fibGenerator();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
