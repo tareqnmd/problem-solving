7ec5b01d0a6076d4e75200b61192f020c217e959
Tareq <tareqnmd@gmail.com>
2024-02-23 18:37:35 +0600

    409

--- Js/409.longest_palindrome.js
+++ Js/409.longest_palindrome.js
@@ -1,24 +1,16 @@
 var longestPalindrome = function (s) {
 	const s_count = {};
 	let highest_palindrome = 0;
-	let highest_odd = 0;
 	for (const c of s) {
 		s_count[c] = (s_count[c] ?? 0) + 1;
 	}
 	for (const item in s_count) {
 		const num = s_count[item];
-		if (num % 2 === 0) {
-			highest_palindrome += num;
-		} else {
-			if (num > 1) {
-				highest_palindrome += num - 1;
-			}
-			if (highest_odd < num) {
-				highest_odd = num;
-			}
-		}
+		highest_palindrome += num % 2 === 0 ? num : num - 1;
 	}
-	return highest_palindrome + (highest_odd ? 1 : 0);
+	return s.length > highest_palindrome
+		? highest_palindrome + 1
+		: highest_palindrome;
 };
 
 console.log(
