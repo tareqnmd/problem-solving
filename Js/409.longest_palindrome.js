0f068b47be65fc9b13aae26e3d6052a6b0ba7856
Tareq <tareqnmd@gmail.com>
2024-02-23 18:40:22 +0600

    up

--- Js/409.longest_palindrome.js
+++ Js/409.longest_palindrome.js
@@ -13,6 +13,20 @@ var longestPalindrome = function (s) {
 		: highest_palindrome;
 };
 
+// var longestPalindrome = function (s) {
+// 	const s_count = {};
+// 	let highest_palindrome = 0;
+// 	for (const c of s) {
+// 		s_count[c] = (s_count[c] ?? 0) + 1;
+// 		if (s_count[c] % 2 === 0) {
+// 			highest_palindrome += 2;
+// 		}
+// 	}
+// 	return s.length > highest_palindrome
+// 		? highest_palindrome + 1
+// 		: highest_palindrome;
+// };
+
 console.log(
 	longestPalindrome(
 		'civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth'
