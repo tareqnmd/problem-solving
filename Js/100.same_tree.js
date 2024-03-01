4e865375145a0cc2c16970e6f1bfaedecf6cc9b3
Tareq <tareqnmd@gmail.com>
2024-03-01 13:12:01 +0600

    up

--- Js/100.same_tree.js
+++ Js/100.same_tree.js
@@ -1,5 +1,5 @@
 var isSameTree = function (p, q) {
-	if (p.length === q.length) return false;
+	if (p.length !== q.length) return false;
 	for (let i = 0; i < p.length; i++) {
 		if (p[i] !== q[i]) {
 			return false;
@@ -8,5 +8,20 @@ var isSameTree = function (p, q) {
 	return true;
 };
 
+var isSameTree = function (p, q) {
+	if (!p && !q) return true;
+	if (!p || !q) return false;
+	return (
+		p.val === q.val &&
+		isSameTree(p.left, q.left) &&
+		isSameTree(p.right, q.right)
+	);
+};
+
+var isSameTree = function (p, q) {
+	return JSON.stringify(p) === JSON.stringify(q);
+};
+
+console.log(isSameTree([1, 2, 3], [1, 2, 3]));
 console.log(isSameTree([1, 2], [1, null, 2]));
-// console.log(isSameTree([1, 2, 3], [1, 2, 3]));
+console.log(isSameTree([1, 2, 1], [1, 1, 2]));
