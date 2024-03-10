d49ca7de5dc4aa9a73dbb49ac0dbec943d293b06
Tareq <tareqnmd@gmail.com>
2024-03-10 17:48:13 +0600

    pascal triangle 2

--- Js/119.pascals_triangle_II.js
+++ Js/119.pascals_triangle_II.js
@@ -1,21 +1,30 @@
+// var getRow = function (rowIndex) {
+// 	const triangle = [];
+// 	for (let i = 0; i <= rowIndex; i++) {
+// 		if (i === 0) {
+// 			triangle.push([1]);
+// 		} else if (i === 1) {
+// 			triangle.push([1, 1]);
+// 		} else {
+// 			const prev_arr = triangle[i - 1];
+// 			const new_item = [1];
+// 			for (let j = 1; j < i; j++) {
+// 				new_item.push(prev_arr[j] + prev_arr[j - 1]);
+// 			}
+// 			new_item.push(1);
+// 			triangle.push(new_item);
+// 		}
+// 	}
+// 	return triangle[rowIndex];
+// };
+
 var getRow = function (rowIndex) {
-	const triangle = [];
-	for (let i = 0; i <= rowIndex; i++) {
-		if (i === 0) {
-			triangle.push([1]);
-		} else if (i === 1) {
-			triangle.push([1, 1]);
-		} else {
-			const prev_arr = triangle[i - 1];
-			const new_item = [1];
-			for (let j = 1; j < i; j++) {
-				new_item.push(prev_arr[j] + prev_arr[j - 1]);
-			}
-			new_item.push(1);
-			triangle.push(new_item);
-		}
+	var ans = new Array(rowIndex + 1);
+	ans[0] = ans[rowIndex] = 1;
+	for (let i = 1; i < rowIndex; i++) {
+		ans[i] = (ans[i - 1] * (rowIndex + 1 - i)) / i;
 	}
-	return triangle[rowIndex];
+	return ans;
 };
 
 console.log(getRow(5));
