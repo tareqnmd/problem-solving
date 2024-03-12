36ceac691c2ec41c775d3960bdf68f7d62bf64f8
Tareq <tareqnmd@gmail.com>
2024-03-12 13:57:42 +0600

    205

--- Js/205.isomorphic_strings.js
+++ Js/205.isomorphic_strings.js
@@ -1,16 +1,26 @@
+// var isIsomorphic = function (s, t) {
+// 	if (s.length !== t.length) return false;
+// 	const char_map_s = {};
+// 	const char_map_t = {};
+// 	for (let i = 0; i < s.length; i++) {
+// 		if (
+// 			(char_map_s[s[i]] && char_map_s[s[i]] !== t[i]) ||
+// 			(char_map_t[t[i]] && char_map_t[t[i]] !== s[i])
+// 		) {
+// 			return false;
+// 		}
+// 		char_map_s[s[i]] = t[i];
+// 		char_map_t[t[i]] = s[i];
+// 	}
+// 	return true;
+// };
+
 var isIsomorphic = function (s, t) {
 	if (s.length !== t.length) return false;
-	const char_map_s = {};
-	const char_map_t = {};
 	for (let i = 0; i < s.length; i++) {
-		if (
-			(char_map_s[s[i]] && char_map_s[s[i]] !== t[i]) ||
-			(char_map_t[t[i]] && char_map_t[t[i]] !== s[i])
-		) {
+		if (s.indexOf(s[i], i + 1) !== t.indexOf(t[i], i + 1)) {
 			return false;
 		}
-		char_map_s[s[i]] = t[i];
-		char_map_t[t[i]] = s[i];
 	}
 	return true;
 };
