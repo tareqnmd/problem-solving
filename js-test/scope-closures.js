326835efd5aae08a92898cb3128509bc25d5e27b
Tareq <tareqnmd@gmail.com>
2024-07-19 11:04:00 +0600

    scope closure q1 q2 added

--- js-test/scope-closures.js
+++ js-test/scope-closures.js
@@ -1,12 +1,42 @@
 // basic
 
-function outer(x) {
-	return function inner(y) {
-		return x * y;
-	};
+// function outer(x) {
+// 	return function inner(y) {
+// 		return x * y;
+// 	};
+// }
+
+// const out = outer(2);
+// console.log(out(3));
+// // or
+// console.log(outer(3)(3));
+
+// q1
+// const outerFunc = () => {
+// 	let count = 0;
+// 	return () => ++count;
+// };
+
+// const counterOne = outerFunc();
+// console.log(counterOne());
+// console.log(counterOne());
+
+// const counterSec = outerFunc();
+// console.log(counterSec());
+// console.log(counterSec());
+
+// q2
+function createCounter() {
+	let globalCount = 0;
+	function incrementCount() {
+		let incrementedValue = ++globalCount;
+		return incrementedValue;
+	}
+
+	return { incrementCount };
 }
 
-const out = outer(2);
-console.log(out(3));
-// or
-console.log(outer(3)(3));
+const counter = createCounter();
+console.log(counter.incrementCount());
+console.log(counter.incrementCount());
+console.log(createCounter().incrementCount());
