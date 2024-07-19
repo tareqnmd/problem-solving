f43fccce232fb04d7ddf737e19a1041635adde3c
Tareq <tareqnmd@gmail.com>
2024-07-19 11:17:41 +0600

    scope closure js test

--- js-test/scope-closures.js
+++ js-test/scope-closures.js
@@ -43,17 +43,33 @@
 
 // q3
 
-function createUserManager() {
-	let user = null;
-	return function (name) {
+// function createUserManager() {
+// 	let user = null;
+// 	return function (name) {
+// 		'use strict';
+// 		user = { name, createdAt: Date.now() };
+// 		return user;
+// 	};
+// }
+
+// const createUser = createUserManager();
+// console.log(createUser === createUser);
+// console.log(createUser('Taiyeba'));
+// console.log(createUser('Taiyeba') == createUser('Taiyeba'));
+// console.log(createUser('Taiyeba') === createUser('Taiyeba'));
+
+// q4
+
+function createCounter(initialCount) {
+	let count = initialCount;
+	return function () {
 		'use strict';
-		user = { name, createdAt: Date.now() };
-		return user;
+		count += 1;
+		return count;
 	};
 }
 
-const createUser = createUserManager();
-console.log(createUser === createUser);
-console.log(createUser('Taiyeba'));
-console.log(createUser('Taiyeba') == createUser('Taiyeba'));
-console.log(createUser('Taiyeba') === createUser('Taiyeba'));
+const counter = createCounter(10);
+counter();
+counter();
+console.log(counter());
